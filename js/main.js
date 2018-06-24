cb6eb90b63686bf21f8152566263a6d5e87ad80e
Bernadett Urban <bernadett.urban@hotmail.com>
2018-06-24 11:21:26 +0200

    feat: MAPBOX API key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoidWJldHRpIiwiYSI6ImNqaXNsczF5czA5aW8za3FtczJnbmo5bTMifQ.AuPyLMc_w6B81I8FSun1Gg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
